eb1997c3ac543cdeac253cdf0c275c9c1bdc200a
Christian Lawrence Perino Rosales <87600251+BossKenshin@users.noreply.github.com>
2022-11-03 03:20:30 +0800

    Add files via upload

--- ogis.sql
+++ ogis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2022 at 04:52 PM
+-- Generation Time: Nov 02, 2022 at 08:19 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -46,6 +46,30 @@ INSERT INTO `admin` (`admin_id`, `admin_username`, `admin_password`, `admin_full
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `assigned_teachers`
+--
+
+CREATE TABLE `assigned_teachers` (
+  `assigned_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `school_year` varchar(50) NOT NULL,
+  `year_level` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `assigned_teachers`
+--
+
+INSERT INTO `assigned_teachers` (`assigned_id`, `teacher_id`, `subject_id`, `course_id`, `school_year`, `year_level`) VALUES
+(1, 5, 4, 2, '2022-2023', '1'),
+(2, 3, 2, 1, '2021-2022', '3'),
+(3, 5, 1, 7, '2020-2021', '4');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `course`
 --
 
@@ -64,7 +88,8 @@ INSERT INTO `course` (`course_id`, `course_name`, `course_abbreviation`, `dept_i
 (1, 'Bachelor of Science in Information Technology', 'BSIT', 5),
 (2, 'Bachelor of Science in Business Administration major in Financial Management', 'BSBA-FM', 2),
 (3, 'Bachelor of Science in Education major in Science', 'BSED-Science', 3),
-(7, 'Bachelor of Science in Computer Science', 'BSSC', 5);
+(7, 'Bachelor of Science in Computer Science', 'BSSC', 5),
+(12, 'Bachelor of Science in Vulcanizing shop', 'BSVS', 5);
 
 -- --------------------------------------------------------
 
@@ -88,9 +113,8 @@ INSERT INTO `department` (`dept_id`, `dept_username`, `dept_password`, `dept_nam
 (2, 'coc', 'coc1', 'College of Commerce', 'COC'),
 (3, 'coe', 'coepass', 'College of Education', 'COE'),
 (5, 'ccs1', 'ccs2', 'College of Computer Studies', 'CCS'),
-(24, 'dmeo2', 'demo2', 'demo1', 'df'),
-(26, 'asdf', 'asdf', 'adsfa\'adf', 'asdf'),
-(27, 'sdaf', 'fasdf', 'asdf&lt;Asdf', 'dasf');
+(24, 'ua', 'ua', 'Unassigned', 'UA'),
+(33, 'resigned', 'resigned', 'Resigned', 'RE');
 
 -- --------------------------------------------------------
 
@@ -104,10 +128,38 @@ CREATE TABLE `student` (
   `student_firstname` varchar(30) NOT NULL,
   `student_lastname` varchar(30) NOT NULL,
   `student_middlename` varchar(30) NOT NULL,
+  `student_deptID` int(11) NOT NULL,
   `student_courseID` int(11) NOT NULL,
-  `student_year` varchar(30) NOT NULL,
-  `student_startYear` varchar(30) NOT NULL
+  `student_startYear` varchar(30) NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `subject_id` int(11) NOT NULL,
+  `subject_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`subject_id`, `subject_name`) VALUES
+(19, 'aaaa'),
+(1, 'Agham'),
+(16, 'asdfdfdfdf'),
+(15, 'asdff'),
+(4, 'demo1'),
+(7, 'fdasdf'),
+(14, 'hehehe'),
+(17, 'hhhhhh'),
+(18, 'klklkl'),
+(12, 'math1'),
+(2, 'Mathematics');
 
 -- --------------------------------------------------------
 
@@ -122,7 +174,7 @@ CREATE TABLE `teacher` (
   `teacher_firstname` varchar(30) NOT NULL,
   `teacher_lastname` varchar(30) NOT NULL,
   `teacher_middlename` varchar(30) NOT NULL,
-  `dept_id` varchar(30) NOT NULL
+  `dept_id` varchar(30) NOT NULL DEFAULT 'Unassigned'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -130,8 +182,10 @@ CREATE TABLE `teacher` (
 --
 
 INSERT INTO `teacher` (`teacher_id`, `teacher_username`, `teacher_password`, `teacher_firstname`, `teacher_lastname`, `teacher_middlename`, `dept_id`) VALUES
-(1, 'teacher1', 'teacher1', 'Loren', 'Ipsum', 'Gets', '1'),
-(2, 'teacher2', 'teacher2', 'Nes', 'Sum', 'Guts', '2');
+(3, 'kyle132', 'kyle132', 'Luke', 'Rosales', 'Perino', '33'),
+(4, 'demo12', 'demo', 'demo1', 'dmeo', 'demo1', '5'),
+(5, 'clr123', 'clr11', 'Christian Lawrence', 'Rosales', 'Perino', '3'),
+(6, 'asfd', 'asdf', 'Kyle', 'Rosales', 'Perino', '2');
 
 --
 -- Indexes for dumped tables
@@ -142,6 +196,15 @@ INSERT INTO `teacher` (`teacher_id`, `teacher_username`, `teacher_password`, `te
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `assigned_teachers`
+--
+ALTER TABLE `assigned_teachers`
+  ADD PRIMARY KEY (`assigned_id`),
+  ADD KEY `teacher_id` (`teacher_id`),
+  ADD KEY `subject_id` (`subject_id`),
+  ADD KEY `course_id` (`course_id`);
 
 --
 -- Indexes for table `course`
@@ -166,6 +229,13 @@ ALTER TABLE `student`
   ADD PRIMARY KEY (`student_id`);
 
 --
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subject_id`),
+  ADD UNIQUE KEY `subject_name` (`subject_name`);
+
+--
 -- Indexes for table `teacher`
 --
 ALTER TABLE `teacher`
@@ -183,28 +253,52 @@ ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `assigned_teachers`
+--
+ALTER TABLE `assigned_teachers`
+  MODIFY `assigned_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `dept_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `dept_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `teacher`
 --
 ALTER TABLE `teacher`
-  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `assigned_teachers`
+--
+ALTER TABLE `assigned_teachers`
+  ADD CONSTRAINT `assigned_teachers_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `assigned_teachers_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  ADD CONSTRAINT `assigned_teachers_ibfk_3` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
